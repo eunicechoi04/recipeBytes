f960d9b0560120d39873ec65d070f3b33dd8e3be
Eunice <eunicec@mit.edu>
2025-01-18 16:53:34 -0500

    env vars loading

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,18 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Load the environment variables from the .env file located in the root folder
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SIGNING_SECRET: process.env.SIGNING_SECRET,
-  },
 };
 
 export default nextConfig;
