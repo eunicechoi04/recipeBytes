7ae7bcfee93f0b814044dd803191aeee0f1d9a75
Eunice Choi <eunicechoi04@gmail.com>
2025-01-19 16:20:03 -0500

    update backend url

--- client/src/context/ApiContext.tsx
+++ client/src/context/ApiContext.tsx
@@ -1,21 +1,17 @@
 "use client";
-import React, { createContext, useContext } from 'react';
-import axios from 'axios';
+import React, { createContext, useContext } from "react";
+import axios from "axios";
 
-import type { AxiosInstance } from 'axios';
+import type { AxiosInstance } from "axios";
 
 const ApiContext = createContext<AxiosInstance | null>(null);
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
   const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Your Flask server URL
+    baseURL: "http://18.116.43.163/api", // Your Flask server URL
   });
 
-  return (
-    <ApiContext.Provider value={api}>
-      {children}
-    </ApiContext.Provider>
-  );
+  return <ApiContext.Provider value={api}>{children}</ApiContext.Provider>;
 };
 
 export const useApi = () => useContext(ApiContext);
